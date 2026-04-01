4a33b047362f4b20ccd8a468c86ce060fa3719c6
2568832 <2568832@cegepmontpetit.ca>
2026-04-01 15:48:21 -0400

    Ex1 Fini

--- Laboratoire9_1-595b7365a136c3a7fb8b324055549619/Laboratoire9_1/Exercices_9.1_A_COMPLETER/Exer01 CheckListBox/FrmPrincipal.cs
+++ Laboratoire9_1-595b7365a136c3a7fb8b324055549619/Laboratoire9_1/Exercices_9.1_A_COMPLETER/Exer01 CheckListBox/FrmPrincipal.cs
@@ -14,27 +14,38 @@ namespace Exemple_CheckListBox
             Text += APP_INFO;
 
             // TODO 01 : Ajouter 10 légumes dans votre CheckListBox
+            string[] list = { "Carotte", "Pomme de tere", "Tomate", "Courgette", "Haricot vert", "Brocoli", "Épinard", "Laitue", "Oignon", "Radis" };
+            clbFruitsEtLegumes.Items.AddRange(list);
 
-            
         }
         private void BtnCocherTout_Click(object sender, EventArgs e)
         {
             // TODO 02 : Cocher tous les items
-
+            for (int i = 0; i < clbFruitsEtLegumes.Items?.Count; i++)
+            {
+                clbFruitsEtLegumes.SetItemChecked(i, true);
+            }
             AfficherLeNombreDItemsCochés();
         }
         //------------------------------------------------------------------------------------------
         private void BtnDécocherTout_Click(object sender, EventArgs e)
         {
             // TODO 03 : Décocher tous les items
-
+            for (int i = 0; i < clbFruitsEtLegumes.Items?.Count; i++)
+            {
+                clbFruitsEtLegumes.SetItemChecked(i, false);
+            }
             AfficherLeNombreDItemsCochés();
         }
         //------------------------------------------------------------------------------------------
         private void BtnCocherIndexPair_Click(object sender, EventArgs e)
         {
             // TODO 04 : Cocher uniquement les items ayant un index pair
-
+            for (int i = 0; i < clbFruitsEtLegumes.Items?.Count; i++)
+            {
+                if ((i%2) == 0)
+                clbFruitsEtLegumes.SetItemChecked(i, true);
+            }
 
             AfficherLeNombreDItemsCochés();
         }
@@ -43,7 +54,7 @@ namespace Exemple_CheckListBox
         private void AfficherLeNombreDItemsCochés()
         {
             // TODO 05 : Afficher dans txtValeur le nombre d'items cochés dans le CheckListBox 
-
+            txtValeur.Text = clbFruitsEtLegumes.CheckedItems.Count.ToString();
 
         }
 
@@ -68,7 +79,13 @@ namespace Exemple_CheckListBox
             string listeDesItemsCochés = string.Empty;
 
             // À COMPLÉTER...
+            
+            foreach (string item in clbFruitsEtLegumes.Items)
+            {
+                listeDesItemsCochés += item + "\n";
+            }
 
+            MessageBox.Show(listeDesItemsCochés);
             return listeDesItemsCochés;
         }
         //------------------------------------------------------------------------------------------
@@ -80,8 +97,12 @@ namespace Exemple_CheckListBox
         private void BtnSupprimer_Click(object sender, EventArgs e)
         {
             // TODO 07 : Supprimer les items cochés en utilisant la propriété CheckedItems
-            
+            for (int i = clbFruitsEtLegumes.CheckedItems.Count - 1; i >= 0; i--)
+            {
+                clbFruitsEtLegumes.Items.Remove(clbFruitsEtLegumes.CheckedItems[i]);
+            }
             AfficherLeNombreDItemsCochés();
+
         }
         //------------------------------------------------------------------------------------------
 
